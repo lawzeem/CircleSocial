c667a83b80da2ba00cfe21f7f04166acde73d35c
Lawzeem <lawzeeml@buffalo.edu>
2020-04-06 20:06:55 -0400

    Other User Profile
    Can look up other users profile. Also there was an error with user creation which did not automatically create the profile since I modified the save function. Its all fixed now.
    To look up a user go to :/profile/user/"Username"

--- cse312/cse312/profile/models.py
+++ cse312/cse312/profile/models.py
@@ -5,11 +5,11 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile')
+    image = models.ImageField(default='profile/default.jpg', upload_to='profile')
     bio = models.CharField('bio', max_length=80)
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
             output_size = (600, 600)
--- cse312/cse312/profile/views.py
+++ cse312/cse312/profile/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import ProfileEditForm
 from .models import Profile
+from cse312.users.models import User
+from django.http import Http404
 
 @login_required
 def showProfile(request):
@@ -20,3 +22,13 @@ def EditProfileView(request):
         form = ProfileEditForm(instance = request.user.profile)
         args = {'form':form, 'user':user}
         return render(request, 'profile/edit.html', args)
+
+@login_required
+def GetProfile(request, username):
+    try:
+        user = User.objects.get(user_name=username)
+        profile = Profile.objects.get(user=user)
+        args = {'profile':profile}
+    except:
+        raise Http404
+    return render(request, 'profile/view.html', args)
