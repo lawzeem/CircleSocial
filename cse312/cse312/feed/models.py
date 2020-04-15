12d11b9d866c9eb2c526a712e601770a6bfadf54
Mckayla Brefka <mckaylab@buffalo.edu>
2020-04-15 16:09:55 -0400

    Post and Comment Model

--- cse312/cse312/feed/models.py
+++ cse312/cse312/feed/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from cse312.users.models import User
+# from cse312.users.models import feed
+from PIL import Image
+
+
+class Post(models.Model):
+    post_id = models.CharField(primary_key=True)
+    title = models.CharField('title', max_length=100)
+    image = models.ImageField(default='post/default.jpg', upload_to='post')
+    description = models.CharField('description', max_length=1000)
+    upvotes= models.IntegerField(default=0)
+    comments = models.CharField('comment', max_length = 2000)
+
+    def save_img(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (600, 600)
+            img.thumbnail(output_size)
+            img.save_img(self.image.path)
+
+    def publish_post(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
+class Comments(models.Model):
+    comment_id = models.CharField(primary_key=True)
+    post_id = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    user_id = models.ForeignKey(User, unique=False)
+    comment = models.TextField()
+    upvotes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.comment
+
 
 # Create your models here.
