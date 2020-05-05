3ff0df55e176a54c82c466897456eb55cd3b841d
vicfal414 <36310124+vicfal414@users.noreply.github.com>
2020-05-04 23:07:11 -0400

    Update models.py for message

--- cse312/cse312/message/models.py
+++ cse312/cse312/message/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from cse312.users.models import User
+from django.utils import timezone
 
 # Create your models here.
+class Message(models.Model):
+    messagetxt = models.CharField('messagetxt', max_length=2000)
+    sender = models.ForeignKey(User, on_delete = models.CASCADE, related_name=('sender'))
+    reciever = models.ForeignKey(User, on_delete = models.CASCADE, related_name=('reciever'))
+    timestamp = models.DateTimeField('timestamp',default=timezone.now)
