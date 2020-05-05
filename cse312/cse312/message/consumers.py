a51aeaa033572664156bc0a789b8ce8996d9fa14
vicfal414 <36310124+vicfal414@users.noreply.github.com>
2020-05-04 23:10:04 -0400

    Created consumers.py
    not completely done yet

---
+++ cse312/cse312/message/consumers.py
@@ -0,0 +1,37 @@
+import asyncio
+import json
+from cse312.users.models import User
+# from django.contrib.auth import get_user_model
+from channels.consumer import AsyncConsumer
+from channels.db import database_sync_to_async
+from .models import Message
+
+class MessageConsumer(AsyncConsumer):
+    async def websocket_connect(self,event):
+        print("connected to messaage consumer",event)
+        await self.send({
+            "type":"websocket.accept"
+        })
+        chatWith = self.scope['url_route']['kwargs']['username']
+        user = self.scope['user']
+        messagesThread = await self.get_messages(user,chatWith)
+        # await asyncio.sleep(30)
+
+    @database_sync_to_async
+    def get_messages(self, user,chatWith):
+        messages = Message.objects.get_or_new(user,chatWith)[0]
+        return message
+
+    async def websocket_recieve(self,event):
+            # print("recieve",event)
+            msgText = event.get('text',None)
+            if msgText is not None:
+                text = json.loads(msgText)
+                messageText = text.get('message')
+                await self.send({
+                    "type":"websocket.send",
+                    "text":messageText
+                })
+
+    async def websocket_disconnect(self,event):
+        print("disconnected from messaage consumer",event)
